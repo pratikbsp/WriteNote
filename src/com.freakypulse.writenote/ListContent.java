216c6ec739943fa4ccd89397f38e3b88dd54d5ef
PRATIK SAXENA <pratikbsp@yahoo.com>
2016-02-01 18:41:29 +0530

    Create ListContent.java

---
+++ src/com.freakypulse.writenote/ListContent.java
@@ -0,0 +1,24 @@
+package com.freakypulse.writenote;
+
+public class ListContent {
+
+	String title,time;
+
+	public ListContent(String title, String time) {
+		super();
+		this.title = title;
+		this.time = time;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+
+	public String getTime() {
+		return time;
+	}
+
+	
+	
+}
