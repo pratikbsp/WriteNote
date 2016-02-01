948460137ca8ae60a9ffe9dd72bc063d120ad5f7
PRATIK SAXENA <pratikbsp@yahoo.com>
2016-02-01 18:40:55 +0530

    Create NavView.java

---
+++ src/com.freakypulse.writenote/NavView.java
@@ -0,0 +1,20 @@
+package com.freakypulse.writenote;
+
+public class NavView {
+	
+	int icon_id;
+	String icon_text;
+	public NavView(int icon_id, String icon_text) {
+		super();
+		this.icon_id = icon_id;
+		this.icon_text = icon_text;
+	}
+	public int getIcon_id() {
+		return icon_id;
+	}
+	public String getIcon_text() {
+		return icon_text;
+	}
+	
+
+}
